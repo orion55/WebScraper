23f66f99b6a80b9587cb608c5f9d37acb8fbe377
Oleg.Grebenev <oleg.grebenev@rokolabs.com>
2021-07-12 10:10:46 +0500

    first commit

---
+++ WebScraper/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace WebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var settings = new Settings();
+            Console.WriteLine(settings.Configuration.GetValue<string>("url"));
+        }
+    }
+}
---
+++ WebScraper/Settings.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace WebScraper
+{
+    public class Settings
+    {
+        public Settings()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("settings.json", optional: false, reloadOnChange: true);
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+    }
+}
