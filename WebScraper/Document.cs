df8c12aad11c9df7e572f2690eb0b22d22db180a
Oleg.Grebenev <oleg.grebenev@rokolabs.com>
2021-07-15 10:10:30 +0500

    Add logger

---
+++ WebScraper/Document.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace WebScraper
+{
+    public class Document
+    {
+        private string _url;
+        private ILogger _log;
+
+
+        public Document(IConfigurationRoot settings, ILogger log)
+        {
+            string url = settings.GetValue<string>("url");
+
+            if (String.IsNullOrEmpty(url))
+                throw new Exception("The \"url\" value is empty");
+
+            this._url = url;
+            this._log = log;
+            
+            this._log.Information("Url: {0}", this._url);
+        }
+    }
+}
--- WebScraper/Program.cs
+++ WebScraper/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Microsoft.Extensions.Configuration;
+using Serilog;
 
 namespace WebScraper
 {
@@ -8,7 +8,27 @@ namespace WebScraper
         static void Main(string[] args)
         {
             var settings = new Settings();
-            Console.WriteLine(settings.Configuration.GetValue<string>("url"));
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            Log.Information("Start scraping...");
+
+            try
+            {
+                Document doc = new Document(settings.Configuration, Log.Logger);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, ex.Message);
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
